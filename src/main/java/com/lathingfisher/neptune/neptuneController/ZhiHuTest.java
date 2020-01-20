254902c303f5ad57cc2aba17f448de93f7ac0f8e
C.Hu <812926136@qq.com>
2020-01-20 18:16:50 +0800

    2020-1-20

--- src/main/java/com/lathingfisher/neptune/neptuneController/ZhiHuTest.java
+++ src/main/java/com/lathingfisher/neptune/neptuneController/ZhiHuTest.java
@@ -1,25 +1,10 @@
 package com.lathingfisher.neptune.neptuneController;
 
-import org.apache.http.Consts;
-import org.apache.http.NameValuePair;
-import org.apache.http.client.CookieStore;
-import org.apache.http.client.config.CookieSpecs;
-import org.apache.http.client.config.RequestConfig;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
-import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.client.protocol.HttpClientContext;
-import org.apache.http.cookie.Cookie;
-import org.apache.http.impl.client.BasicCookieStore;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.message.BasicNameValuePair;
-import org.apache.http.util.EntityUtils;
+import com.alibaba.fastjson.JSONObject;
 
-import java.io.IOException;
-import java.util.LinkedList;
-import java.util.List;
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 /**
  * @auther mxh
@@ -27,77 +12,36 @@ import java.util.List;
  */
 public class ZhiHuTest {
 
-    public static void main(String[] args) throws java.text.ParseException {
+    public static void main(String[] args) throws IOException {
 
-        String name = "******@qq.com"; //
-        String password = "******"; //
-
-        // 全局请求设置
-        RequestConfig globalConfig = RequestConfig.custom().setCookieSpec(CookieSpecs.STANDARD).build();
-        // 创建cookie store的本地实例
-        CookieStore cookieStore = new BasicCookieStore();
-        // 创建HttpClient上下文
-        HttpClientContext context = HttpClientContext.create();
-        context.setCookieStore(cookieStore);
-
-        // 创建一个HttpClient
-        CloseableHttpClient httpClient = HttpClients.custom().setDefaultRequestConfig(globalConfig)
-                .setDefaultCookieStore(cookieStore).build();
-
-        CloseableHttpResponse res = null;
-
-        // 创建本地的HTTP内容
-        try {
-            try {
-                // 创建一个get请求用来获取必要的Cookie，如_xsrf信息
-                HttpGet get = new HttpGet("http://www.zhihu.com/");
-
-                res = httpClient.execute(get, context);
-                // 获取常用Cookie,包括_xsrf信息
-                System.out.println("访问知乎首页后的获取的常规Cookie:===============");
-                for (Cookie c : cookieStore.getCookies()) {
-                    System.out.println(c.getName() + ": " + c.getValue());
-                }
-                res.close();
-
-                // 构造post数据
-                List<NameValuePair> valuePairs = new LinkedList<NameValuePair>();
-                valuePairs.add(new BasicNameValuePair("email", name));
-                valuePairs.add(new BasicNameValuePair("password", password));
-                valuePairs.add(new BasicNameValuePair("remember_me", "true"));
-                UrlEncodedFormEntity entity = new UrlEncodedFormEntity(valuePairs, Consts.UTF_8);
-                entity.setContentType("application/x-www-form-urlencoded");
-
-                // 创建一个post请求
-                HttpPost post = new HttpPost("https://www.zhihu.com/login/email");
-                // 注入post数据
-                post.setEntity(entity);
-                res = httpClient.execute(post, context);
-
-                // 打印响应信息，查看是否登陆是否成功
-                System.out.println("打印响应信息===========");
-                System.out.println(res.toString());
-                res.close();
-
-                System.out.println("登陆成功后,新的Cookie:===============");
-                for (Cookie c : context.getCookieStore().getCookies()) {
-                    System.out.println(c.getName() + ": " + c.getValue());
-                }
-
-                // 构造一个新的get请求，用来测试登录是否成功
-                HttpGet newGet = new HttpGet("https://www.zhihu.com/question/293207596/answer/684673400");
-                res = httpClient.execute(newGet, context);
-                String content = EntityUtils.toString(res.getEntity());
-                System.out.println("登陆成功后访问的页面===============");
-                System.out.println(content);
-                res.close();
-
-            } finally {
-                httpClient.close();
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
+        String urls = "https://api.live.bilibili.com/room/v1/room/get_user_recommend?page=0";
+        URL url = new URL(urls);
+        //打开和url之间的连接
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        PrintWriter out = null;
+        //请求方式
+        conn.setRequestMethod("GET");
+        //设置通用的请求属性
+        conn.setRequestProperty("accept", "*/*");
+        conn.setRequestProperty("connection", "Keep-Alive");
+        conn.setRequestProperty("Content-Type", "application/json;charset=utf-8");
+        //设置是否向httpUrlConnection输出，设置是否从httpUrlConnection读入，此外发送post请求必须设置这两个
+        //最常用的Http请求无非是get和post，get请求可以获取静态页面，也可以把参数放在URL字串后面，传递给servlet，
+        //post与get的 不同之处在于post的参数不是放在URL字串里面，而是放在http请求的正文内。get请求不需要设置
+//        conn.setDoOutput(true);
+//        conn.setDoInput(true);
+        //获取URLConnection对象对应的输出流
+        conn.connect();
+        //获取URLConnection对象对应的输入流
+        InputStream is = conn.getInputStream();
+        //构造一个字符流缓存
+        BufferedReader br = new BufferedReader(new InputStreamReader(is));
+        String str;
+        JSONObject jsonss = null;
+        while ((str = br.readLine()) != null) {
+            jsonss = JSONObject.parseObject(str);
         }
+        System.out.println("获取到的报文数据为：" + jsonss);
     }
 
 }
